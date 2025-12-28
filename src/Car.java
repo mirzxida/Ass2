4933523e3ba3ea948d5577351dc0e15a090f0155
Hilola <mirzabekovahilola14@gmail.com>
2025-12-28 19:19:23 +0500

    Car class is ready

---
+++ src/Car.java
@@ -0,0 +1,19 @@
+public class Car extends Vehicle{
+    private int doors;
+    private String fuelType;
+
+    public Car(String brand, int year, int doors, String fuelType){
+        super(brand, year);
+        this.doors = doors;
+        this.fuelType = fuelType;
+    }
+
+    @Override
+    public void startEngine(){
+        System.out.println(brand+ " car is started.");
+    }
+
+    public void stopEngine(){
+        System.out.println(brand+ " car is stopped.");
+    }
+}

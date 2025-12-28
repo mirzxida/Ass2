15d0a6128528a255d020dd6e93a056e86383db4a
Hilola <mirzabekovahilola14@gmail.com>
2025-12-28 19:45:50 +0500

    Motorcycle class is ready

---
+++ src/Motorcycle.java
@@ -0,0 +1,17 @@
+public class Motorcycle extends Vehicle{
+    private boolean hasSidecar;
+
+    public Motorcycle(String brand, int year, boolean hasSidecar){
+        super(brand, year);
+        this.hasSidecar = hasSidecar;
+    }
+
+    @Override
+    public void startEngine(){
+        System.out.println(brand+ " motorcycle is started.");
+    }
+
+    public void stopEngine(){
+        System.out.println(brand+ " motorcycle is stopped.");
+    }
+}

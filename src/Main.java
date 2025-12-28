8ce56406f258862b7d450a638bc79acd7d3e4f01
Hilola <mirzabekovahilola14@gmail.com>
2025-12-29 03:16:11 +0500

    Main class is done

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,24 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-  //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-  // to see how IntelliJ IDEA suggests fixing it.
-  IO.println(String.format("Hello and welcome!"));
+public class Main {
+    public static void main(String[] args){
+        Driver dr_John = new Driver("John", "P1112223");
+        Driver dr_Tox = new Driver("Tox", "M6452215");
 
-  for (int i = 1; i <= 5; i++) {
-    //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-    // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-    IO.println("i = " + i);
-  }
+        Car car1 = new Car("Lexus", 2006, 4, "Petrol");
+        Motorcycle moto1 = new Motorcycle("Honda", 2004, false);
+        Truck truck1 = new Truck("Volvo", 1994, 10.5, 4);
+
+        car1.setDriver(dr_John);
+        moto1.setDriver(dr_Tox);
+        truck1.setDriver(dr_Tox);
+
+        Vehicle[] garage = {car1, moto1, truck1};
+
+        System.out.println("Info:");
+
+        for (Vehicle v: garage){
+            v.startEngine();
+            v.displayInfo();
+            v.stopEngine();
+        }
+    }
 }
